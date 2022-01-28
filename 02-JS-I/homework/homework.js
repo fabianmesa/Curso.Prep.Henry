a05919f76502bf81f460df9e1a7b2a12a23f0c1a
Carlos Fabian Mesa <80798770+Fabianmesa1124@users.noreply.github.com>
2022-01-27 19:25:12 -0500

    js-1- passed

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -210,6 +210,9 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+
+  mostrar = nombre+" "+apellido;
+  return mostrar;
   
 }
 
@@ -217,12 +220,15 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  saludo = "Hola " + nombre + "!";
+  return saludo;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  area = alto * ancho; 
+  return area;
   
 }
 
@@ -230,14 +236,16 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  perimetro = lado * 4;
+  return perimetro;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  var area=(base * altura)/2;
+  return area; 
 }
 
 
@@ -245,6 +253,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+  var conversion = euro * 1.20;
+  return conversion; 
   
 }
 
@@ -255,16 +265,20 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  if(letra.length>1){
+    return "Dato incorrecto";
+  }
+  else if(letra==="a" || letra==="e" || letra==="i" || letra==="o" || letra==="u" ){
+    return "Es vocal";
+  }
+  else {
+    return "Dato incorrecto";
+  }
 
-
-  
 }
-
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
-
 module.exports = {
   nuevaString,
   nuevoNum,
